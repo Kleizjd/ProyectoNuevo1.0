9d60775c6e12dd6cc3e1a30c889b7bc92535f8d3
JuanDaGo <juandavidgo1997@gmail.com>
2022-02-18 14:13:40 -0500

    tanks

--- Views/Template/header_tienda.php
+++ Views/Template/header_tienda.php
@@ -47,10 +47,4 @@
     </nav>
     <!-- //Navbar -->
 </div>
-<!-- Carrusel de imgenes -->
-<?php include_once "views/Template/Modals/carousel.php" ?>
-<!-- Iframe google maps-->
-<div class="map">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7965.468764380914!2d-76.5569245!3d3.41478!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sco!4v1587511385606!5m2!1ses!2sco" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-</div>
-<!-- //Iframe google maps-->
+
