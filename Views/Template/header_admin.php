7540116bbb38fead6f5bb0334ed686e33e9ffd35
JuanDaGo <juandavidgo1997@gmail.com>
2022-02-19 17:40:24 -0500

    best

--- Views/Template/header_admin.php
+++ Views/Template/header_admin.php
@@ -117,6 +117,7 @@
                         <!-- ============================================================== -->
                         <!-- User profile and search -->
                         <!-- ============================================================== -->
+                        <!-- <li class="nav-item right-side-toggle"> <a class="nav-link  waves-effect waves-light" href="javascript:void(0)"><i class="ti-settings"></i></a></li> -->
                         <li class="nav-item right-side-toggle"> <a class="nav-link  waves-effect waves-light" href="javascript:void(0)"><i class="ti-settings"></i></a></li>
                     </ul>
                 </div>
