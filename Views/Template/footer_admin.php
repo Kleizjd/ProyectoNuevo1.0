b558076fe708cd8418ec9ef65e170dead9fc9040
JuanDaGo <juandavidgo1997@gmail.com>
2022-02-19 17:46:47 -0500

    framework 90%

--- Views/Template/footer_admin.php
+++ Views/Template/footer_admin.php
@@ -1,6 +1,10 @@
-<footer class="footer">
+<!-- <footer class="footer">
     © 2020 Elegent Admin by <a href="https://www.wrappixel.com/">wrappixel.com</a>
-</footer>
+</footer> -->
+<div class="footer">
+    <!-- <p>2017 Empresa. All Rights Reserved | Design by brothers Grijalba </a></p> -->
+    &copy;Copyright <?= date("Y"); ?> Best -José Daniel Grijalba
+</div>
 <!-- ============================================================== -->
 <!-- End footer -->
 <!-- ============================================================== -->
@@ -10,33 +14,33 @@
 <!-- ============================================================== -->
 <!-- ============================================================== -->
 <!-- All Jquery -->
-   <!-- ============================================================== -->
-   <script src="<?= media() ?>/node_modules/jquery/jquery-3.2.1.min.js"></script>
-    <!-- Bootstrap popper Core JavaScript -->
-    <script src="<?= media() ?>/node_modules/popper/popper.min.js"></script>
-    <script src="<?= media() ?>/node_modules/bootstrap/js/bootstrap.min.js"></script>
-    <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="<?= media() ?>/template/dist/js/perfect-scrollbar.jquery.min.js"></script>
-    <!--Wave Effects -->
-    <script src="<?= media() ?>/template/dist/js/waves.js"></script>
-    <!--Menu sidebar -->
-    <script src="<?= media() ?>/template/dist/js/sidebarmenu.js"></script>
-    <!--Custom JavaScript -->
-    <script src="<?= media() ?>/template/dist/js/custom.min.js"></script>
-    <!-- ============================================================== -->
-    <!-- This page plugins -->
-    <!-- ============================================================== -->
-    <!--morris JavaScript -->
-    <script src="<?= media() ?>/node_modules/raphael/raphael.min.js"></script>
-    <script src="<?= media() ?>/node_modules/morrisjs/morris.min.js"></script>
-    <script src="<?= media() ?>/node_modules/jquery-sparkline/jquery.sparkline.min.js"></script>
-    <!--c3 JavaScript -->
-    <script src="<?= media() ?>/node_modules/d3/d3.min.js"></script>
-    <script src="<?= media() ?>/node_modules/c3-master/c3.min.js"></script>
-    <!-- Popup message jquery -->
-    <script src="<?= media() ?>/node_modules/toast-master/js/jquery.toast.js"></script>
-    <!-- Chart JS -->
-    <script src="<?= media() ?>/template/dist/js/dashboard1.js"></script>
+<!-- ============================================================== -->
+<script src="<?= media() ?>/node_modules/jquery/jquery-3.2.1.min.js"></script>
+<!-- Bootstrap popper Core JavaScript -->
+<script src="<?= media() ?>/node_modules/popper/popper.min.js"></script>
+<script src="<?= media() ?>/node_modules/bootstrap/js/bootstrap.min.js"></script>
+<!-- slimscrollbar scrollbar JavaScript -->
+<script src="<?= media() ?>/template/dist/js/perfect-scrollbar.jquery.min.js"></script>
+<!--Wave Effects -->
+<script src="<?= media() ?>/template/dist/js/waves.js"></script>
+<!--Menu sidebar -->
+<script src="<?= media() ?>/template/dist/js/sidebarmenu.js"></script>
+<!--Custom JavaScript -->
+<script src="<?= media() ?>/template/dist/js/custom.min.js"></script>
+<!-- ============================================================== -->
+<!-- This page plugins -->
+<!-- ============================================================== -->
+<!--morris JavaScript -->
+<script src="<?= media() ?>/node_modules/raphael/raphael.min.js"></script>
+<script src="<?= media() ?>/node_modules/morrisjs/morris.min.js"></script>
+<script src="<?= media() ?>/node_modules/jquery-sparkline/jquery.sparkline.min.js"></script>
+<!--c3 JavaScript -->
+<script src="<?= media() ?>/node_modules/d3/d3.min.js"></script>
+<script src="<?= media() ?>/node_modules/c3-master/c3.min.js"></script>
+<!-- Popup message jquery -->
+<script src="<?= media() ?>/node_modules/toast-master/js/jquery.toast.js"></script>
+<!-- Chart JS -->
+<script src="<?= media() ?>/template/dist/js/dashboard1.js"></script>
 
 </body>
 
