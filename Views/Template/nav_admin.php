61df92f5978263133fef67e7f0ef0068449d9738
JuanDaGo <juandavidgo1997@gmail.com>
2022-02-20 00:04:18 -0500

    here we go

--- Views/Template/nav_admin.php
+++ Views/Template/nav_admin.php
@@ -8,6 +8,12 @@
             <a class="nav-lock waves-effect waves-dark ml-auto hidden-md-down" href="javascript:void(0)"><i class="mdi mdi-toggle-switch"></i></a>
             <a class="nav-toggler waves-effect waves-dark ml-auto hidden-sm-up" href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
     </div>
+    <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="<?= media(); ?>/images/avatar.png" alt="User Image">
+        <div>
+            <p class="app-sidebar__user-name">Jose</p>
+            <p class="app-sidebar__user-designation">Administrador</p>
+        </div>
+    </div>
     <!-- Sidebar scroll-->
     <div class="scroll-sidebar">
         <!-- Sidebar navigation-->
@@ -23,9 +29,9 @@
                 <li> <a class="waves-effect waves-dark" href="map-google.html" aria-expanded="false"><i class="app-menu__icon fas fa-envelope"></i><span class="hide-menu"></span>Mensajes</a></li>
                 <li> <a class="waves-effect waves-dark" href="pages-blank.html" aria-expanded="false"><i class="app-menu__icon fas fa-file-alt"></i><span class="hide-menu"></span>Paginas</a></li>
                 <li> <a class="waves-effect waves-light" href="pages-blank.html" aria-expanded="false"><i class="fa fa-power-off text-success"></i>
-                <span class="hide-menu"></span>Salir</a></li>
-                
-               
+                        <span class="hide-menu"></span>Salir</a></li>
+
+
 
                 <!-- <div class="text-center m-t-30"> -->
                 <!-- <li>
